c04aa75f8eddb08a6844a876c30e8a60e8d1e86b
Александр <sashayatsukov@gmail.com>
2020-10-29 02:26:55 +0300

    Начал работать с ролями

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.example.controller;
+
+import com.example.domain.Role;
+import com.example.domain.User;
+import com.example.repos.UserRepo;
+import com.sun.tools.javac.code.Attribute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model){
+        model.addAttribute("users", userRepo.findAll());
+        return "userList";
+    }
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+    @PostMapping
+    public String userSave(@RequestParam String username, @RequestParam Map<String, String> form, @RequestParam("userId") User user) {
+             user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        for (String key: form.keySet()){
+            if (roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+             userRepo.save(user);
+        return "redirect:/user";
+    }
+}
