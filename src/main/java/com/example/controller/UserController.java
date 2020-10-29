d5d17d0a6b4b257c20aaa787c1a53e115b1bc450
Александр <sashayatsukov@gmail.com>
2020-10-29 15:01:31 +0300

    Admin and roles

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,20 +3,22 @@ package com.example.controller;
 import com.example.domain.Role;
 import com.example.domain.User;
 import com.example.repos.UserRepo;
-import com.sun.tools.javac.code.Attribute;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.Arrays;
-import java.util.Comparator;
+
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepo userRepo;
@@ -33,11 +35,12 @@ public class UserController {
         return "userEdit";
     }
     @PostMapping
-    public String userSave(@RequestParam String username, @RequestParam Map<String, String> form, @RequestParam("userId") User user) {
-             user.setUsername(username);
+    public String userSave(@RequestParam String userName, @RequestParam Map<String, String> form, @RequestParam("userId") User user) {
+             user.setUsername(userName);
         Set<String> roles = Arrays.stream(Role.values())
                 .map(Role::name)
                 .collect(Collectors.toSet());
+        user.getRoles().clear();
         for (String key: form.keySet()){
             if (roles.contains(key)){
                 user.getRoles().add(Role.valueOf(key));
